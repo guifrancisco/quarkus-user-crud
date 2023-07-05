fa88e9260a3e1953c1101ced02890cf7436348b4
guifrancisco <guifrancisco4@gmail.com>
2023-07-04 21:02:04 -0300

    feat: :tada: quarkus user crud

---
+++ src/main/java/io/github/guifrancisco/quarkussocial/rest/dto/CreateUserRequest.java
@@ -0,0 +1,15 @@
+package io.github.guifrancisco.quarkussocial.rest.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class CreateUserRequest {
+
+    @NotBlank(message = "Name is required")
+    private String name;
+    @NotNull(message = "Age is required")
+    private Integer age;
+
+}
